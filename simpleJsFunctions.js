4a4d8517e9cdef12ba7fbcd1efe5e14d06cc3345
Geraldo Novais <geraldo.novais@gmail.com>
2023-02-28 18:33:16 -0300

    initial commit

---
+++ simpleJsFunctions.js
@@ -0,0 +1,27 @@
+
+/**
+ * Converts the first character of a string to uppercase
+ * 
+ * @return {string} The new string with the first character in uppercase
+ *             
+ */
+String.prototype.ucfirst = function () {
+    return this.charAt(0).toUpperCase() + this.substr(1);
+};
+
+/**
+ * Converts a string into camel case, removing hyphens, underscores and spaces.
+ * 
+ * @param {text} string Text to camelize
+ * @return string converted into camel case 
+ */
+var camelize = function (text) {
+    return text.replace(/^([A-Z])|[\s-_]+(\w)/g, function (match, p1, p2, offset) {
+        if (p2)
+            return p2.toUpperCase();
+        return p1.toLowerCase();
+    }).ucfirst();
+}
+
+// Exporting variables and functions
+export { camelize };
